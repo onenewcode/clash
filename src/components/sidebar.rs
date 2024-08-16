3d9c5807c552beff1d6b5e4fa2975431ca975b4c
alphonsezhang <lovestudy@qq.com>
2024-08-16 11:00:05 +0800

    sidebar

--- src/components/sidebar.rs
+++ src/components/sidebar.rs
@@ -1,81 +1,58 @@
-use std::sync::{atomic::AtomicUsize, mpsc::Receiver};
+use std::{sync::{atomic::AtomicUsize, mpsc::Receiver}, usize};
 use eframe::egui::{
-    self,  text::LayoutJob,  Color32, FontFamily, FontId, Label,
- TextFormat, 
+    self, text::LayoutJob, Button, Color32, FontFamily, FontId, Label, TextFormat 
 };
 pub const SIDE_BAR: &str = "side_bar";
 // 侧边栏按钮的状态
+#[derive(Clone)]
 enum State {
-    Default,
-    General,
+    Checked,
+    UnChecked,
 }
 #[derive(Default)]
-pub struct SideBar {
+pub struct SideBar{
     // 侧边栏,标头名称
     title: String,
-    // 按钮列表
-    bottons: Vec<egui::Button>,
     // 按钮名称列表
-    botton_names: Vec<String>,
+    button_names: Vec<String>,
     // 按钮状态
     states:Vec<State>,
 }
-impl SideBar {
+impl  SideBar{
+    // 渲染侧边栏
     pub fn show(&mut self, ctx: &egui::Context) {
+        // 生成侧边Panel
         let side_panel = egui::SidePanel::left(SIDE_BAR);
+        // 禁止侧边栏拖动，
         side_panel.resizable(false).show(ctx, |ui| {
-            ui.add(self.upload_lable());
-            let mut page = Vec::with_capacity(5);
-            page.push((State::Default, egui::Button::new("Default")));
-            page.push((State::General, egui::Button::new("General")));
-            for i in page {
-                let (k, v) = i;
-                if ui.add(v).clicked() {
-                    self.state = k;
-                }
+          // 渲染标题
+          // 逐个渲染按钮
+        (0..self.button_names.len()).into_iter().for_each(|i|{
+            match self.states[i] {
+                State::Checked => {
+                    ui.button(self.button_names[i].clone()).highlight();
+                },
+                State::UnChecked => {
+                   if ui.button(self.button_names[i].clone()).clicked(){
+                    self.refresh_button(i);
+                   }
+                },
             }
-            ui.menu_button("My sub-menu", |ui| {
-                if ui.button("Close the menu").clicked() {
-                    ui.close_menu();
-                }
-            });
+        })
         });
     }
-    fn upload_lable(&self) -> Label {
-        let mut job = LayoutJob::default();
-        let mut speed = format!("0k \n,0k");
-        if self.rbool == true {
-            match self.receiver.try_recv() {
-                Ok((up,down)) => speed = format!("{}k \n,{}k", up, down),
-                Err(r) =>{
-                    format!("{}",r);
-                },
-            };
-        }
-        job.append(
-            &speed,
-            0.0,
-            TextFormat {
-                font_id: FontId::new(14.0, FontFamily::Proportional),
-                color: Color32::WHITE,
-                ..Default::default()
-            },
-        );
-        egui::Label::new(job)
+    // 用于更新按钮被被点击后的状态,index为按钮下标
+    fn refresh_button(&mut self,index:usize){
+        self.states.fill(State::UnChecked);
+        self.states[index] = State::Checked;
     }
-    pub fn open_receiver(&mut self) {
-        self.rbool = true;
-    }
-    pub fn close_receiver(&mut self) {
-        self.rbool = false;
-    }
-    pub fn new(title: String, receiver: Receiver<(f32, f32)>) -> Self {
-        let mut page = Vec::with_capacity(5);
-        page.push((State::Default, egui::Button::new("Default")));
+    
+    pub fn new(title: String, names:Vec<String>) -> SideBar  {
+        let states = vec![State::UnChecked;names.len()];
         Self {
-            // c:ctx,
             title: title,
-            ..Default::default()
+           button_names:names,
+           states:states,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-#![allow(rustdoc::missing_crate_level_docs)] // it's an example
-
 use eframe::egui;
-
+use clash::components::sidebar;
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
@@ -11,49 +8,25 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "Confirm exit",
         options,
-        Box::new(|_cc| Ok(Box::<MyApp>::default())),
+        Box::new(|_cc| Ok(Box::new(MyApp::new_test()))),
     )
 }
 
 #[derive(Default)]
 struct MyApp {
-    show_confirmation_dialog: bool,
-    allowed_to_close: bool,
+    side_bar: sidebar::SideBar,
 }
 
 impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Try to close the window");
-        });
-
-        if ctx.input(|i| i.viewport().close_requested()) {
-            if self.allowed_to_close {
-                // do nothing - we will close
-            } else {
-                ctx.send_viewport_cmd(egui::ViewportCommand::CancelClose);
-                self.show_confirmation_dialog = true;
-            }
-        }
-
-        if self.show_confirmation_dialog {
-            egui::Window::new("Do you want to quit?")
-                .collapsible(false)
-                .resizable(false)
-                .show(ctx, |ui| {
-                    ui.horizontal(|ui| {
-                        if ui.button("No").clicked() {
-                            self.show_confirmation_dialog = false;
-                            self.allowed_to_close = false;
-                        }
-
-                        if ui.button("Yes").clicked() {
-                            self.show_confirmation_dialog = false;
-                            self.allowed_to_close = true;
-                            ui.ctx().send_viewport_cmd(egui::ViewportCommand::Close);
-                        }
-                    });
-                });
-        }
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        self.side_bar.show(ctx);
+    }
+}
+impl MyApp {
+    //
+    pub fn new_test()->Self{
+        let side_bar = vec!["test1".to_string(),"test2".to_string()];
+        
+        MyApp{side_bar:sidebar::SideBar::new("test".to_string(),side_bar)}
     }
 }
